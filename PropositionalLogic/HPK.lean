32da67dff865c0c9f0aa1ce35771a9addd46e596
SnO2WMaN <me@sno2wman.net>
2023-07-26 08:50:03 +0900

    Some instantinate in HPK

--- PropositionalLogic/HPK.lean
+++ PropositionalLogic/HPK.lean
@@ -5,6 +5,7 @@ import PropositionalLogic.HPM
 namespace PropositionalLogic
 
 open Finset Notation HilbertSystem
+attribute [-simp] union_assoc
 
 class HPK
   (L : Type u) [DecidableEq L] [HilbertSystem L]
@@ -19,6 +20,25 @@ variable
   [HilbertSystem L] [HPK L]
 
 open HilbertSystem HPM₀ HPM
+open HasAxiomK HasMP HasDNE
+
+instance : HasCon₃ L := ⟨HasCon₃.axiomCon₃⟩
+instance : HasCon₄ L := ⟨HasCon₄.axiomCon₄⟩
+
+instance : HasEFQ L where
+  axiomEFQ Γ φ := by
+    simp [deduction];
+    have h1 : Γ ∪ {⊥'} ⊢ (⊥' : L) := by simp;
+    have h2 : Γ ∪ {⊥'} ⊢ ((φ →' ⊥') →' ⊥') →' φ := by rw [←eqLnot, ←eqLnot]; apply axiomDNE;
+    have h3 : Γ ∪ {⊥'} ⊢ (⊥' →' (φ →' ⊥') →' ⊥') := by apply axiomK;
+    have h4 := MP h3 h1;
+    have h5 := MP h2 h4;
+    assumption;
+
+instance : HasTND L where
+  axiomTND Γ φ ψ := by admit
+
+instance : HasCM₂ L := ⟨HasCM₂.axiomCM₂⟩
 
 end HPK
 
