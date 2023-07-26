4b774916c7ff97f6b63a39ba13f1eb9db4cdec87
SnO2WMaN <me@sno2wman.net>
2023-07-26 12:23:59 +0900

    Update lean toolchain & mathlib4

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ lean_lib «PropositionalLogic» {
   -- add library configuration options here
 }
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
 
 @[default_target]
 lean_exe «propositional-logic» {
